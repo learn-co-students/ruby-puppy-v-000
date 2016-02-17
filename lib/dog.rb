f304fa76a02a897f901d51acbef1a06a72101e09
wsaxe <geosaxe@gmail.com>
2016-02-16 19:11:52 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @@all << self
+    @name = name
+  end
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  def self.clear_all
+    @@all.clear
+  end
+
+end
