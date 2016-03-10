62d91f02b9ff4e6753986d17401bd0672a71050d
rafeeq-123 <rafeeq-123@hotmail.com>
2016-03-09 17:16:44 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,54 @@
+class Dog
+@@all=[]
+attr_accessor :name
+
+def initialize(name)
+  @name=name
+  @@all << self
+end
+
+def self.all
+
+   @@all.each do |dog_name| puts dog_name.name
+
+   end
+
+end
+
+def self.clear_all
+
+  if @@all.any?
+
+    @@all.clear
+
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
