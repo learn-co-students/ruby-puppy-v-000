bf6dcc3922d64c4276a9209a9b61eda6e432e4ff
Juan Rivera <juanriv45@gmail.com>
2016-06-27 04:44:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  @@all = []
+  attr_accessor  :name
+
+  def initialize(name)
+    @@all << self
+    @name = name
+  end
+
+  def self.all
+      puts @@all
+  end
+  def self.clear_all
+    @@all.clear
+  end
+end
