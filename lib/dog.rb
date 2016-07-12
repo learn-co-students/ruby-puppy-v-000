0e7df586a7b04ea192fac5adc6a0744d655aabee
andrewmmarquis <andrewmmarquis@gmail.com>
2016-07-12 14:46:41 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |puppy|
+      puts puppy.name
+    end
+  end
+  
+
+
+end
