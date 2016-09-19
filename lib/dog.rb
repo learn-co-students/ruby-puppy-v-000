ff835fc4eba9c3f12c565c4f3672b27148d75866
bc86 <michael.4486@gmail.com>
2016-09-18 23:32:30 -0400

    class methods and pup names printed

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each { |pups| puts pups.name }
+  end
+
+end
