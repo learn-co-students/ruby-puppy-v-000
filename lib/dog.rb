7f753433871a699aa436c85b1b75efd986ecada1
Elias Ventura <eliasventura23@gmail.com>
2017-01-15 23:19:13 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,13 +9,13 @@ class Dog
     @@all << self
   end
 
-  def self.clear_all
-    @@all.clear
-  end
-
   def self.all
     @@all.each do |dog|
       puts dog.name
+    end
   end
-end
+
+  def self.clear_all
+    @@all.clear
+  end
 end
