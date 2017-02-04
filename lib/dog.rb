eefbd3a9d1ea42cfb391c350720d3b322194d5ae
charlie lee <ctlee9096@gmail.com>
2017-02-04 22:15:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,14 +6,16 @@ def initialize(name)
   @name = name
   @@all << self
 end
-def self.all?
-  #binding.pry
-  @@all.each do |name|
-    binding.pry
-    puts "#{name}"
-  end
 def self.clear_all
   @@all.clear
 end
+def self.all
+  #binding.pry
+  @@all.each do |dog|
+  #binding.pry
+    puts dog.name #used instance method,.name, to send receiver, dog, the
+    #value of the class variable, @@all, array
+  #binding.pry
+  end
 end
 end
