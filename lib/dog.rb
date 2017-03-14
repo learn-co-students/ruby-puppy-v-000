65998d9fc51ac7e6f400672389eb2e47231bb0f4
Alex Crisan <alexcrisan2005@hotmail.com>
2017-03-14 15:44:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.collect do |puppy|
+      puts puppy.name
+    end
+    # binding.pry
+  end
+
+  def self.name
+    puppy
+  end
+end
