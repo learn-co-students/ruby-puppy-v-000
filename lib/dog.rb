3663e116b2b7a30b8ff6eff6338f501305a6017f
Joseph Hyatt <jhyatt1017@gmail.com>
2017-04-17 20:10:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog
-  #set a class variable @@all equal to an empty array inside your class
+  #has a class variable, @@all, the points to an array
 	@@all = []
 	attr_accessor :name
-
+#initializes with an argument of a name, adds the new dog to the @@all array
 	def initialize(name)
 		@name = name
 		@@all << self
 	end
-
+#is a class method that empties the @@all array of all existing dogs
 	def self.clear_all
 		@@all.clear
 	end
-#method .all, that iterates over all of the individual dogs stored in the @@all array and puts out their name to the terminal.
+#is a class method that puts out the name of each dog to the terminal
 	def self.all
 		@@all.each { |dog| puts dog.name }
 	end
