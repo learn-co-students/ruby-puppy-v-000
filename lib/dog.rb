98048cda165ce05bc468a1fb36f45150ba6e5b90
Michael Mirzakandov <mikemirzak@aol.com>
2017-04-23 20:39:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+   @@all.each do |dog|
+     puts dog.name
+   end
+  end
+
+
+end
