acf365d7d9f9fcd3a0986b0d0588f103ad310a2a
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-05-07 21:45:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+     
+    attr_accessor :name
+    @@all = [] 
+ 
+ #this initilizes with a name and pushes each new dogs name into the @@all array
+     def initialize(name)
+          @name = name
+          @@all << self
+     end
+     #this puts out the name of each dog to the termainal
+     def self.all
+          @@all.each do |dog|
+               puts dog.name
+          end
+         
+     end
+     
+     #this clears the array of existing dogs
+     def self.clear_all
+          @@all = []
+     end
+end
