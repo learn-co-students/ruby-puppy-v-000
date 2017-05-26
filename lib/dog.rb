b9b82f101d730e03a3106ea6e17c841c3892837c
Joseph Kim <josephkim0224@gmail.com>
2017-05-26 22:27:12 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each { |e| puts e.name }
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+end
