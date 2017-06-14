2598e02c66d365bb832143df1b03ae9594068614
Meredith Reece <nerdybug@gmail.com>
2017-06-14 23:41:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all.each do |dog_instance|
+      puts dog_instance.name
+    end
+  end
+end
