2d3694c78b7fba8d12d90cf82864684c2068765e
Jeremy <emata@rapidratings.com>
2017-06-14 21:47:29 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,22 @@
 class Dog
 
-attr_accessor :name
+  attr_accessor :name
 
-@@all = []
+  @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
   end
-  
-  def self.all 
-    @@all.each do |dog|
-      puts dog.name
+
+  def self.clear_all
+    @@all = @@all.clear
+  end
+
+  def self.all
+    @@all.each do |pet|
+      puts pet.name
     end
   end
 
-  def self.clear_all
-    @@all.clear
-  end
-  
 end
