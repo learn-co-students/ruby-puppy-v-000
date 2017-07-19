a02872bf6ae3c7d37a29f7b131174f9f9a288f2d
Isabel Vazquez <12ivazquez@gmail.com>
2017-07-19 02:55:04 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |song|
+      puts song.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
