9a3a0e2a92bf3517e5c57dfc73a832b5daaf6bd1
Yosa Yon <yosa.yon@gmail.com>
2017-10-16 22:22:03 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  @@all = []
+
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |puppies|
+      puts puppies.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
