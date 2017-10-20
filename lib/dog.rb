93a9b721dc0752d5503375153365190f38cbfc3f
Julianne Kim <juliannne@gmail.com>
2017-10-20 19:49:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  attr_reader :name
+  attr_accessor :name
 
   @@all = []
 
@@ -9,12 +9,15 @@ class Dog
   end
 
   def self.clear_all
-    @@all = []
+    # @@all = []
+    @@all.clear
   end
 
   def self.all
     @@all.each do |dog|
       puts dog.name
     end
+    # puts @@all.map { |dog| dog.name}
+    # or puts @@all.map(&:name)
   end
 end
