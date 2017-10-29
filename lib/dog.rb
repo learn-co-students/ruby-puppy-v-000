578431bb4119c5b81204bdcc0a2881fecd9c4b79
Araba Taylor <araba.taylor20@gmail.com>
2017-10-29 15:43:55 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+require "pry"
+
+class Dog
+
+  @@all = []
+  attr_accessor :name
+
+
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+
+
+  def self.all
+    @@all.each {|puppy| puts puppy.name}
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
