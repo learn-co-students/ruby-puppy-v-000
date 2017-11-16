b43d851d3df7506b1b10da5ceccf0f189075dd20
charityyoungblood <youngblood.charity@gmail.com>
2017-11-15 16:16:23 -0800

    added comments for how to structure project

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+#create class Dog 
+#
+#create initialize method with: 
+#  - name
+#
+#create class variable @@all with empty array as value
+#  
+#create class method for:
+#  - clear_all
+#  - all
+#  - new 
+#  
+#create instance method for name 
