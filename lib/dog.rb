6e18ace879e24037fbab3c24e8e8477207222f18
William Hicks <w.hicks917@gmail.com>
2017-12-13 17:49:26 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+require "pry"
+
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each {|instance| puts instance.name}
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
