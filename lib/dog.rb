d2794f95384647bb5e567b8b77ad78514c885e23
rh24 <ms.rebeccahong@gmail.com>
2017-12-20 16:27:03 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,8 +8,10 @@ class Dog
   end
 
   def self.all
-    @@all.each {|dog| puts dog.name}
-  end
+    @@all.each {|dog| puts dog.name} # We need dog.name because "dog" only refers to
+  end                                # instances of dogs that are being shoveled into the array,
+                                     # and we want to puts the dog instance's actual name.
+                                     # To do this we use a reader method on each dog instance.
 
   def self.clear_all
     @@all.clear
