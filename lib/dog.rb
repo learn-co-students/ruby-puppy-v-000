9fb21b1aa53d27f6388e26f0242dbd6cdf3689b4
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-11 16:41:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,6 +11,10 @@ attr_accessor :name
     @@all.each do |puppy|
       puts puppy.name
     end
+    # could also be written as
+    # puts @@all.map { |puppy| puppy.name}
+    # or
+    #puts @@all.map(&:name)--- but this is more complex than is necessary
   end
 
   def self.clear_all
