df3cc72327171e4cb96d02843462adbc3eb7538a
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-15 15:43:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all.each do |dogs|
+      puts dogs.name
+    end
+  end
+  
+  def self.clear_all
+    @@all.clear
+    
+  end
+  
+  
+end
