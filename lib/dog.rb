1feea8662aa3e68cfc454cca410a2715fa2474a9
Zac Willmington <zac.willmington@gmail.com>
2018-02-17 01:03:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,15 +3,19 @@ class Dog
 
       @@all = []
 
-      def initializes(name)
+      def initialize(name)
             @name = name
             @@all << self
       end
 
       def self.all
             @@all.each do  |dog|
-                  dog.name
+                  puts dog.name
             end
+      end
+
+      def self.clear_all
+            @@all.clear
       end
 
 
