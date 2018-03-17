4ea9d1cd23ea588994769efc6f55c145d0717b18
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-17 12:25:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,18 @@
 class Dog
-  
-  @@all = []
-  
   attr_accessor :name
-  
-  def initialize(name) 
+
+  @@all = []
+
+  def initialize(name)
     @name = name
-  end
-  
-  def self.new(name)
     @@all << self
   end
-  
+
+  def self.all
+    puts @@all.collect{ |dog| dog.name }
+  end
+
   def self.clear_all
     @@all.clear
-  end
-  
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
-    end
   end
 end
