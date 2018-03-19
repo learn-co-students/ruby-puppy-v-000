0f5e7c37dc1770e0e28c91c897b6cc095aaa2694
Maverick Tesini <mavericktesini@gmail.com>
2018-03-19 17:56:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,18 +3,19 @@ class Dog
  attr_accessor :name
 
  @@all = []
- @@dog_names = []
+ # @@dog_names = []
 
  def initialize(name)
     @name = name
-    @@dog_names << name
+    # @@dog_names << name
     @@all << self
     # binding.pry
  end
 
  def self.all
-   x = @@dog_names.uniq
-   x.each {|dog| puts "#{dog}"}
+  #  x = @@dog_names.uniq               ##these solutions were too bloated.
+  #  x.each {|dog| puts "#{dog}"}  #instead of using a new string, use method of object.
+  puts @@all.collect {|dog| dog.name}  #this works, huzzah. More concise.
  end
 
  def self.clear_all
