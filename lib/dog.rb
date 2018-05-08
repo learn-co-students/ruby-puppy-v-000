6178cd64fe8653559c437c4c4a6fa272fb67a5af
Eric Fiorani <efiorani305@gmail.com>
2018-05-08 19:00:14 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+    puts dog.name
+    end
+  end
+
+  def self.clear_all
+    if @@all.length >= 1
+      @@all.clear
+    end
+  end
+
+end
