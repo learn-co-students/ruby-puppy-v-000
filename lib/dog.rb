707f9191b56248340fedeee7d6f3ae2eb07f3aad
Ethel C <cofresi.ethel@gmail.com>
2018-07-04 18:57:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+
+    @name = name
+    @@all << self
+
+  end
+
+  def self.all
+
+    @@all.each do |dog|
+      puts dog.name
+    end
+
+  end
+
+  def self.clear_all
+
+    @@all.clear
+
+  end
+
+end
+
+#TESTING 
+  Dog.clear_all
+  
+  Dog.new("Pluto")
+  Dog.new("Fido")
+  Dog.new("Maddy")
+  
+  Dog.all
+  
