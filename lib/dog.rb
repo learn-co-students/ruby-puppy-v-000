f66458d2327cbf9fbdcbda621f0b63bd434c0e60
Steven Crouse <sdcrouse91@gmail.com>
2018-08-31 07:47:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,6 +10,8 @@ class Dog
   
   def self.all 
     @@all.each {|dog| puts dog.name}
+    # This technically works, but it returns the @@all array.
+    # If I want this to return nothing, then puts @@all.collect {...} or puts @@all.map {...}
   end 
   
   def self.clear_all
