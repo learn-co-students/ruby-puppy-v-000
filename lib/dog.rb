ad3a83a8505a4c9ffd68700693b7f0f71cbd71c3
Dan Suarez <suarez.daniel101@gmail.com>
2019-03-25 17:13:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
+
+# class Song
+#
+#   @@all = []
+#
+#   attr_accessor :name
+#
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+# end
