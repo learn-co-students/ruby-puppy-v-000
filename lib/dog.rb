cc71a5babec5eb3d387255b72a8a1f1d802a6411
Tiffany <ong.tifffany1503@gmail.com>
2019-09-20 23:58:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Add your code here
+class Dog
+  attr_accessor :name
+
+   @@all = []
+
+   def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+   def self.clear_all
+    @@all.clear
+  end
+
+   def self.all
+    @@all.each do |d|
+      puts d.name
+    end
+
+    def .print_all
+      @@all
+      puts d.name
+    end
+  end
+end
